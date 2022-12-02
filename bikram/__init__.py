fabb071acab632c925a4f80b5f6ad2b9806d511a
acpmasquerade <acp.dhruba@gmail.com>
2022-12-02 13:39:06 +0545

    some minor text changes

--- bikram/__init__.py
+++ bikram/__init__.py
@@ -1,5 +1,11 @@
 from .bikram import *   # NOQA
 
-__author__ = """keshaB Paudel"""
+"""
+Originally created and maintained by keshaB Poudel (github.com/poudel), 
+now backend and maintained by Aerawat Corp Pvt. Ltd. (github.com/aerawatcorp)
+- December, 2022
+"""
+
+__author__ = """keshaB Poudel"""
 __email__ = 'self@keshab.net'
 __version__ = '2.0.0'
